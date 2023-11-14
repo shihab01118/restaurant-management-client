042d69c03921fb7d80b65eb88e3c6ab0190f5034
shihab01118 <shahed.shihab.0@gmail.com>
2023-11-14 20:18:57 +0600

    firebase config added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,23 +1,43 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  validateCaptcha,
+} from "react-simple-captcha";
 import login_img from "../../assets/others/authentication2.png";
 import "./Login.css";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [disabled, setDisabled] = useState(true);
+  const captchaRef = useRef();
 
-    const handleLogin = e => {
-        e.preventDefault();
-        console.log(email, password);
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, []);
+
+  const handleValidateCaptcha = () => {
+    const userCaptchaValue = captchaRef.current.value;
+    if (validateCaptcha(userCaptchaValue)) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
     }
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
 
   return (
     <section className="min-h-screen login  py-10 md:py-16 lg:py-24">
       <div className="max-w-6xl mx-8 md:mx-16 lg:mx-auto shadow-2xl px-24 py-14 flex flex-col md:flex-row items-center">
         <div className="md:w-1/2">
-          <img src={login_img} alt="login" className="w-3/4" />
+          <img src={login_img} alt="login" className="w-4/5" />
         </div>
-        <div className="md:w-1/2">
+        <div className="md:w-1/2 lg:mr-12">
           <h2 className="text-5xl font-bold text-center mb-6">Login</h2>
           <form onSubmit={handleLogin}>
             <div className="form-control">
@@ -40,12 +60,30 @@ const Login = () => {
                 type="password"
                 placeholder="Your Password"
                 className="input input-bordered"
-                onBlur={e => setPassword(e.target.value)}
+                onBlur={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <LoadCanvasTemplate />
+              </label>
+              <input
+                type="text"
+                placeholder="Enter the text above"
+                className="input input-bordered"
+                ref={captchaRef}
+                onBlur={handleValidateCaptcha}
                 required
               />
             </div>
             <div className="form-control mt-6">
-              <button className="btn bg-[#dbb984] text-white capitalize text-xl font-bold">Login</button>
+              <button
+                disabled={disabled}
+                className="btn bg-[#dbb984] text-white capitalize text-xl font-bold"
+              >
+                Login
+              </button>
             </div>
           </form>
         </div>
