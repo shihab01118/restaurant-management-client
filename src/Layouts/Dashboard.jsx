28ea0706abc09076a40bad27d785fd0fb24d7579
shihab01118 <shahed.shihab.0@gmail.com>
2023-11-16 02:49:53 +0600

    sidebar added to dashboard and cart page added

--- src/Layouts/Dashboard.jsx
+++ src/Layouts/Dashboard.jsx
@@ -1,16 +1,81 @@
+import {
+  FaAd,
+  FaCalendar,
+  FaFax,
+  FaHamburger,
+  FaHome,
+  FaList,
+  FaShoppingBag,
+  FaShoppingCart,
+} from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
   return (
     <div className="flex">
-      <div className="w-64 bg-[#D1A054] min-h-screen p-5">
-        <ul className="menu">
+      <div className="w-64 bg-[#D1A054] min-h-full p-5">
+        <h2 className="text-3xl font-bold text-center mb-5">
+          Bistro Boss <br /> <span className="font-semibold">Restaurant</span>
+        </h2>
+        <ul className="menu space-y-2">
           <li>
-            <NavLink to="/dashboard/cart">My Cart</NavLink>
+            <NavLink to="/dashboard/userHome">
+              <FaHome />
+              User Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/reservation">
+              <FaCalendar />
+              Reservation
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/cart">
+              <FaShoppingCart />
+              My Cart
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/review">
+              <FaAd />
+              Add Review
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/booking">
+              <FaList />
+              My Booking
+            </NavLink>
+          </li>
+          <div className="divider"></div>
+          <li>
+            <NavLink to="/">
+              <FaHome />
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/">
+              <FaHamburger />
+              Menu
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/">
+              <FaShoppingBag />
+              Shop
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/">
+              <FaFax />
+              Contact
+            </NavLink>
           </li>
         </ul>
       </div>
-      <div className="flex-1">
+      <div className="flex-1 pt-8 pb-16 bg-base-200">
         <Outlet />
       </div>
     </div>
--- src/pages/Dashboard/Cart/Cart.jsx
+++ src/pages/Dashboard/Cart/Cart.jsx
@@ -1,9 +1,24 @@
-const Cart = () => {
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useCart from "../../../hooks/useCart";
 
-    return (
-        <div>
-            <h2 className="text-6xl font-bold text-center">My Cart</h2>
+const Cart = () => {
+  const { cart } = useCart();
+  const totalPrice = cart.reduce((sum, item) => item.price + sum, 0);
+  return (
+    <section>
+      <div className="">
+        <SectionTitle heading="WANNA ADD MORE?" subheading="My Cart" />
+      </div>
+      <div className="bg-white mx-16 p-10">
+        <div className="flex justify-between">
+          <h2 className="text-3xl font-bold">Total Orders: {cart.length}</h2>
+          <h2 className="text-3xl font-bold">Total Price: ${totalPrice}</h2>
+          <button className="btn bg-[#D1A054] border-none capitalize text-white text-lg font-bold">
+            Pay
+          </button>
         </div>
-    )
+      </div>
+    </section>
+  );
 };
 export default Cart;
